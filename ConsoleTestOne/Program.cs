c0edeeff2e6d8a6ef8aa638bf95ab791c4163f53
Eric Nordin <gitprojectjockey@users.noreply.github.com>
2020-07-25 10:48:47 -0600

    Removed bug FooBar

--- ConsoleTestOne/Program.cs
+++ ConsoleTestOne/Program.cs
@@ -7,17 +7,6 @@ namespace ConsoleTestOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(Foo.GetFooBar());
         }
-
-        public static class Foo
-        {
-            public static string GetFooBar()
-            {
-                return "foobar";
-            }
-        }
-
-
     }
 }
